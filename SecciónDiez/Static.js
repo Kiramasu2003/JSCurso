7b7d491b2d47bb1e5915346fe7bca7ba0623c4ec
Kiramasu2003 <sarcer1800@alumno.ipn.mx>
2021-09-25 05:44:05 -0500

    6

--- SecciónDiez/Static.js
+++ SecciónDiez/Static.js
@@ -1,9 +1,16 @@
 //Con clases no aplica el concepto de hoisting
 class Persona{
+
+    static contadorObjetoPersona=0;//Atributo de la clase
+
+    email = 'Valor default email'; //Atributo de nuestros obj
+
     constructor(nombre, apellidoUno) {
         //private _nombreVariables
         this._nombre = nombre;
         this._apellidoUno = apellidoUno;
+        Persona.contadorObjetoPersona++;
+        console.log('Se incrementa contador');
     }
 
     get nombre(){
@@ -29,13 +36,24 @@ class Persona{
     toString(){
         return this.nombreCompleto();
     }
+    static saludar(){
+        console.log("Saludos desde static")
+    }
 
+    static saludar2(persona){
+        console.log(persona.nombre + ' ' + persona.apellidoUno);
+    }
 }
 
 class Empleado extends Persona{
+
+    static contadorObjetoEmpleado=0;
+
     constructor(nombre, apellido, departamento) {
         super(nombre, apellido);
         this._departamento = departamento;
+        Empleado.contadorObjetoEmpleado++;
+        console.log('Se incrementa contador Empleado');
     }
     get departamento(){
         return this._departamento;
@@ -58,6 +76,8 @@ console.log(persona1.apellidoUno);
 persona1.nombre = 'Juan Carlos';
 console.log(persona1)
 //persona1.saludar(); no es posible llamar un metodo static desde un obj
+Persona.saludar();
+Persona.saludar2(persona1);
 
 
 let persona2 = new Persona('Carlos', 'Lara');
@@ -69,5 +89,13 @@ console.log(empleado1);
 console.log(empleado1.nombreCompleto());
 console.log(empleado1.toString());
 
+Empleado.saludar();
+Empleado.saludar2(empleado1);
 
+console.log(Persona.contadorObjetoPersona);
+console.log(Empleado.contadorObjetoPersona);
+console.log(Empleado.contadorObjetoEmpleado);
 
+console.log(persona1.email);
+console.log(empleado1.email);
+console.log(Persona.email);//Es undefined pq es el valor por default
